1e10147ad99858bf2146b410e1d7cc1c340aa76e
m-danish-pv <90462284+m-danish-pv@users.noreply.github.com>
2022-06-27 17:36:17 +0500

    Add files via upload

---
+++ Final batch.bat
@@ -0,0 +1,5 @@
+C:\Users\muhammad.danish\AppData\Local\Programs\Python\Python310\python.exe c:\import_csv.py %*
+D:
+cd Java Projects
+java -jar test.jar
+C:\Users\muhammad.danish\AppData\Local\Programs\Python\Python310\python.exe c:\NPRA_query_into_df.py %*
